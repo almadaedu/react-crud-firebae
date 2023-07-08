e2b946cb59145c05a4dde15e72750c8a3fe036df
Eduardo Almada <56881337+almadaedu@users.noreply.github.com>
2023-07-08 10:44:05 -0300

    :sparkles: Added edit button to cards

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Button from '~/components/button';
 import Input from '~/components/input';
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
 
 export type Tool = {
   id: string;
@@ -126,13 +127,14 @@ function Index() {
             {tools.map((tool) => (
               <div
                 key={tool.id}
-                className="h-48 flex flex-col justify-between rounded-md shadow-slate-900 shadow-md p-4 bg-gradient-to-r from-slate-800 to-slate-700"
+                className="h-48 l group relative justify-between rounded-md shadow-slate-900 shadow-md p-4 bg-gradient-to-r from-slate-800 to-slate-700"
               >
                 <div> 
                 <div className="text-xl mb-2 font-bold">{tool.title}</div>
                 <div className="">{tool.description}</div>
                 </div>
                 <a className="text-slate-400" href={tool.url}>{tool.url}</a>
+                <PencilSquareIcon className='h-6 w-6 text-blue-600 hidden group-hover:block absolute right-4 top-4 cursor-pointer '/>
               </div>
             ))}
           </div>
